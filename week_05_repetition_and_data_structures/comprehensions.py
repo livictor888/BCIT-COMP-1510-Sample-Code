ccfebe753da1f74b7cf65ce4ee0f3854223aff9c
Chris <cthompson98@bcit.ca>
2021-02-14 16:11:30 -0800

    Fixed bug for upper bound.

--- week_05_repetition_and_data_structures/comprehensions.py
+++ week_05_repetition_and_data_structures/comprehensions.py
@@ -1,12 +1,13 @@
 """
-Experiment with comprehensions."""
+Experiment with comprehensions.
+"""
 
 
 def main():
     counts = []
     for value in range(26):
         counts.append(0)
-    letters = [chr(letter) for letter in range(65, 92)]
+    letters = [chr(letter) for letter in range(65, 91)]
     tally = dict(zip(letters, counts))
     print(tally)
     tally_2 = {letter: count for letter in letters for count in counts}
