c758c3a0656c200e834a0661f5d29b59f2aa1369
Chris <chris_thompson@bcit.ca>
2022-03-22 21:10:36 -0700

    Move two programs from week 10 to week 12 where they belong.

--- week_09_while_dictionaries_iteration/itertools_demos.py
+++ week_09_while_dictionaries_iteration/itertools_demos.py
@@ -6,34 +6,34 @@ import itertools
 
 
 def main():
-    names = ["Arthur", "Eryn", "Inid", "Oliver", "Ursula"]
-    pairs = []
-    for pair in zip(itertools.count(1), names):
-        pairs.append(pair)
-    print(pairs)
+    # names = ["Arthur", "Eryn", "Inid", "Oliver", "Ursula"]
+    # pairs = []
+    # for pair in zip(itertools.count(1), names):
+    #     pairs.append(pair)
+    # print(pairs)
+    #
+    # evens = itertools.count(step=2)
+    # even_numbers = []
+    # for _ in range(10):
+    #     even_numbers.append(next(evens))
+    # print(even_numbers)
+    #
+    # count_with_floats = itertools.count(start=0.5, step=0.75)
+    # float_sequence = []
+    # for _ in range(5):
+    #     float_sequence.append(next(count_with_floats))
+    # print(float_sequence)
+    #
+    # choices = []
+    # boolean_generator = itertools.cycle([True, False])
+    # for _ in range(20):
+    #     choices.append(next(boolean_generator))
+    # print(choices)
 
-    evens = itertools.count(step=2)
-    even_numbers = []
-    for _ in range(10):
-        even_numbers.append(next(evens))
-    print(even_numbers)
-
-    count_with_floats = itertools.count(start=0.5, step=0.75)
-    float_sequence = []
-    for _ in range(5):
-        float_sequence.append(next(count_with_floats))
-    print(float_sequence)
-
-    choices = []
-    boolean_generator = itertools.cycle([True, False])
-    for _ in range(20):
-        choices.append(next(boolean_generator))
-    print(choices)
-
-    permutations = list(itertools.permutations(['cyan', 'magenta', 'yellow']))
-    print("There are %d permutations: " % len(permutations))
-    for sequence_number, permutation in enumerate(permutations, 1):
-        print("%d:\t%s" % (sequence_number, permutation))
+    # permutations = list(itertools.permutations(['cyan', 'magenta', 'yellow']))
+    # print("There are %d permutations: " % len(permutations))
+    # for sequence_number, permutation in enumerate(permutations, 1):
+    #     print("%d:\t%s" % (sequence_number, permutation))
 
     size = 2
     combinations = list(itertools.combinations(["ginger", "allspice", "cumin", "mint"], size))
